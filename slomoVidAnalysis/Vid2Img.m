6a3c8595fc7b97efff76ce78280ae6493badd761
Matthew <mstanley3@zagmail.gonzaga.edu>
2016-07-12 15:54:37 -0700

    Made small changes to Vid2Img.m Tested that function and make sure that it works: 	Works, though takes a long time to run on larger videos.

--- slomoVidAnalysis/Vid2Img.m
+++ slomoVidAnalysis/Vid2Img.m
@@ -1,28 +1,28 @@
-function Vid2Img( vidName, vidStruct, backName, calParam)
+function vidStruct = Vid2Img( vidName, vidStruct, backName, calParam)
 %The purpose of this function is take images from a video and put it into a structure
 %   Input:  vidName(String): Name of the interested video
 %           vidStruct(Structure): Structure to output video in **
 %           backName(String): Name of background image
 %           calParam(int): Parameters of for camera calibration
 %
-%   Output: No output, but adds to structures
+%   Output: vidStruct(Structure): The same structure as the input
 %
 %   *vidStruct must follow conventions listed in instructions
 
     video = vision.VideoFileReader(vidName);
-
-    if ~isempty(backName)
+    
+    if exist('backName','var') == 1
         background = imread(backName);
     end
 
     n = 1;
     while ~isDone(video)
         currentImage = step(video);
-        if ~isempty(calParam)
+        if exist('calParam','var') == 1
             [currentImage,~] = undistortImage(currentImage,calParam);
         end
         currentImage = rgb2gray(currentImage);
-        if ~isempty(backName)
+        if exist('backName','var') == 1
             currentImage = currentImage - background;
         end
         vidStruct(n).image = currentImage;
