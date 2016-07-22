7f2d899ebcfb65b18f65f7cee68f224f5a41eff1
Matthew <mstanley3@zagmail.gonzaga.edu>
2016-07-22 15:26:14 -0700

    Continued to separate functions from my Ncorr attempt. Though not tested yet, functions RemoveBackground.m and Vid2Img.m are copys from the tested files in Coner/Edge branch.

---
+++ slomoVidAnalysis/LabFunction.m
@@ -0,0 +1,7 @@
+function [ output_args ] = LabFunction( input_args )
+%UNTITLED10 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
--- slomoVidAnalysis/Vid2Img.m
+++ slomoVidAnalysis/Vid2Img.m
@@ -1,11 +1,8 @@
 function vidStruct = Vid2Img( vidName, vidStruct)
 %The purpose of this function is take images from a video and put it into a structure
 %   Input:  vidName(String): Name of the interested video
-%           vidStruct(Structure): Structure to output video in **
 %
 %   Output: vidStruct(Structure): The same structure as the input
-%
-%   *vidStruct must follow conventions listed in instructions
 
     video = vision.VideoFileReader(vidName);
 
