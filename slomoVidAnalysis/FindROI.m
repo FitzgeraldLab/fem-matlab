cf8095ae1fe9a8f2359c766852186c8150277313
Matthew <mstanley3@zagmail.gonzaga.edu>
2016-07-27 10:36:32 -0700

    Added the DIC files as stated before (not all the files were commited)

---
+++ slomoVidAnalysis/FindROI.m
@@ -0,0 +1,18 @@
+function vidStruct = FindROI( vidStruct, thresh )
+%The purpose of this function is to find the region of interest on each frame
+%Assumes that background has been subtracted from the image (background is black
+%   Input:  vidStruct(Structure): Structure to output video in **
+%           thresh(int): the tresh value (0-1) for turning gray scale to bw
+%
+%   Output: vidStruct(Structure): The same structure as the input
+    
+    for i = 1:length(vidStruct)
+        image = vidStruct(i).image;
+        if exist('thresh', 'var') == 0
+            thresh = graythresh(image);
+        end
+        bw = im2bw(image, thresh);
+        bw = imdilate(bw, strel('disk',2));
+        vidStruct(i).roi = bw;
+    end    
+end
