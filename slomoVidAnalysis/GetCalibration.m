9ba9461d045dbeffeea9cc1cec18cd6d96aa05aa
Matthew <mstanley3@zagmail.gonzaga.edu>
2016-07-29 13:04:19 -0700

    After testing, had to change GetCalibration to save some images from the video into the folder in order to use the detectcheckerboardpoint function.  Also expect values for length conversion to be wrong as images used have not been corrected yet.

--- slomoVidAnalysis/GetCalibration.m
+++ slomoVidAnalysis/GetCalibration.m
@@ -1,31 +1,44 @@
-function [ params, lengthConvert ] = GetCalibration( calibrateName, checkerLength, boardLength )
+function [ params, lengthConvert ] = GetCalibration( calibrateName, checkerLength, boardLength, current )
 %The purpose of this function is to get the information to undistort images
 %   Input:  calibrateName(String): name of the video used to get calibration parameters
 %           checkerLength(int): the length of the sides of the squares in the checker pattern
 %           boardLength(int Array): the total height then wide of the checker pattern
+%           current(string): location of the file you are using this program in
 %
 %   Output: params(int): the information to undistort images
 %           lengthConvert(int): multiply to pixel distance to get world distance
 %               becomes array if height and width conversion are not the same
+%
+%   Need to have folder CalImages in your current folder
 
+    calLocation = fullfile(current,'CalImages');
     video = vision.VideoFileReader(calibrateName);
-    images = struct('images',{});
     count = 0;
+    i = 15;
+    cd(calLocation);
+    delete(calLocation)
     while ~isDone(video)
         count = count + 1;
         currentImage = step(video);
-        currentImage = rgb2gray(currentImage);
-        images(count).images = currentImage;
+        % Only saves so many images based on the number in i
+        if rem(count,i) == 0
+            num = count/i;
+            currentImage = rgb2gray(currentImage);
+            filename = sprintf('image%d.jpg',num);
+            fullfilenames{num} = fullfile(calLocation,filename);
+            imwrite(currentImage,filename,'jpg');
+        end
     end
-    [imagePoints, boardSize] = detectCheckerboardPoints(images.images);
-    worldPoints = generateCheckerboardPoints(boardSize, checkerLength);
-    params = estimageCameraParameters(imagePoints, worldPoints);
+    [imagePoints, boardSize] = detectCheckerboardPoints(fullfilenames);
     deltaY = boardLength(1)/boardSize(1);
     deltaX = boardLength(2)/boardSize(2);
-    if round(deltaY,-2) == round(deltaX,-2)
-        lengthConvert = round(deltaX,-2);
+    if round(deltaY,2) == round(deltaX,2)
+        lengthConvert = round(deltaX,2);
     else
         lengthConvert = [deltaY,deltaX];
     end
+    worldPoints = generateCheckerboardPoints(boardSize, checkerLength);
+    params = estimateCameraParameters(imagePoints, worldPoints);
+    cd(current)
 end
 
--- slomoVidAnalysis/LabFunction.m
+++ slomoVidAnalysis/LabFunction.m
@@ -1,4 +1,4 @@
-function [ output ] = LabFunction( calibrateName, checkerLength, boardLength )
+function [ output ] = LabFunction( vidname, backName, calibrateName, checkerLength, boardLength )
 %The purpose of this function is to have one file to perform the lab
 %   
 
@@ -20,6 +20,17 @@ function [ output ] = LabFunction( calibrateName, checkerLength, boardLength )
     %%Ncorr Commands
     handles_ncorr.set_ref(vidCell(:,:,1));
     handles_ncorr.set_cur(vidCell);
+    handles_ncorr.set_roi_ref(vidROI(:,:,1));
+    handles_ncorr.set_roi_cur(vidROI);
     
+    if length(lengthConvert) == 1
+        message = 'Use the following as the pixel to length conversion: ' + int2str(lengthConvert);
+        disp(message)
+    else
+        message = 'Use the following as the pixel to length conversion along x axis: ' + int2str(lengthConver(1));
+        disp(message)
+        message = 'Use the following as the pixel to length conversion along y axis: ' + int2str(lengthConver(2));
+        disp(message)
+    end
 end
 
