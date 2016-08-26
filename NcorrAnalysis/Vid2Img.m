e158416c760b1289a68f74cc2b0c26672e1a287e
Matthew <mstanley3@zagmail.gonzaga.edu>
2016-08-26 14:38:18 -0700

    Allowed Vid2Img to only go through as many frames as you specify.

--- NcorrAnalysis/Vid2Img.m
+++ NcorrAnalysis/Vid2Img.m
@@ -1,14 +1,18 @@
-function vidCell = Vid2Img( vidName, div)
+function vidCell = Vid2Img( vidName, div, lastNum)
 %The purpose of this function is take images from a video, turn it to uint8,and put it into a structure
 %   Input:  vidName(String): Name of the interested video
 %           div(int): Save frame if its number is divisible by this number
+%           lastNum(int): The number of the last frame you wish to look at
 %
 %   Output: vidCell(Cell): Cell created to hold all images
 
     video = vision.VideoFileReader(vidName);
     
-    if exist('div', 'var') == 0;
+    if exist('div', 'var') == 0
         div = 1;
+    end
+    if exist('lastNum', 'var') == 0
+        lastNum = -1;
     end
     
     count = 0;
@@ -19,5 +23,11 @@ function vidCell = Vid2Img( vidName, div)
             currentImage = uint8(currentImage);
             vidCell{1,1,(count/div)} = currentImage;
         end
+        if count == lastNum
+            display('Ending')
+            return
+            displaying('Did not stop properly')
+        end
+        display(count);
     end
 end
