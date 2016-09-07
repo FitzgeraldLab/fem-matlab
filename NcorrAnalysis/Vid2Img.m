0a27613220c1c70d1c55772daabc3a68b1c8a499
Matthew <mstanley3@zagmail.gonzaga.edu>
2016-09-06 18:20:25 -0700

    Made Vid2Img.m so that it would go through the entire video once to see how big to make vidCell.  After allocating the data, it then goes through the video again to add images to vidCell.

--- NcorrAnalysis/Vid2Img.m
+++ NcorrAnalysis/Vid2Img.m
@@ -8,6 +8,7 @@ function vidCell = Vid2Img( vidName, div, lastNum)
 
     video = vision.VideoFileReader(vidName);
     
+    %If div and lastNum are not state when calling the function
     if exist('div', 'var') == 0
         div = 1;
     end
@@ -15,19 +16,31 @@ function vidCell = Vid2Img( vidName, div, lastNum)
         lastNum = -1;
     end
     
+    %Runs the first time to see how many frames it has
     count = 0;
     while ~isDone(video)
         count = count + 1;
-        currentImage = step(video);
-        if rem(count,div) == 0
-            currentImage = uint8(currentImage);
-            vidCell{1,1,(count/div)} = currentImage;
-        end
-        if count == lastNum
-            display('Ending')
-            return
-            displaying('Did not stop properly')
-        end
-        display(count);
+        step(video);
     end
+    
+    %Create a cell for holding images
+    vidCell = cell(1,1,count/div);
+    release(video)
+    video.PlayCount = 1;
+    
+    %Reread video
+    i = 0;
+    while ~isDone(video)
+        i = i + 1;
+        currentImage = step(video);
+        if rem(i,div) == 0
+            currentImage = uint8(currentImage);
+            vidCell{1,1,(i/div)} = currentImage;
+        end
+        if i == lastNum
+            release(video)
+            return
+        end
+    end
+    release(video)
 end
