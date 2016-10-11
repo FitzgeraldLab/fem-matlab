7ca10166d5494337ddc1ff8e33fb92ca0bad6224
34-Matt <mstanley3@zagmail.gonzaga.edu>
2016-10-11 10:56:36 -0700

    Since the clips we took did not combine into one video created a function to go through a file and finds all the videos.  Not finished, but am going to work on it on another computer.

---
+++ NcorrAnalysis/Folder2Img.m
@@ -0,0 +1,10 @@
+function vidCell = Folder2Img( fileName )
+%The purpose of this function is take images from a series of video in a folder, turn it to uint8,and put it into a structure
+%   Input:  fileName(String): Name of the folder with the vid
+%
+%   Output: vidCell(Cell): Cell created to hold all images
+
+    
+
+end
+
