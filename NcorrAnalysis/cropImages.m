8c514e8570ba916b818c7bfca1f5677543da0072
34-Matt <mstanley3@zagmail.gonzaga.edu>
2016-11-18 18:21:21 -0800

    Made cropImages.m in order to crop one image in a cell and apply that crop to all the other images in the cell.

---
+++ NcorrAnalysis/cropImages.m
@@ -0,0 +1,16 @@
+function vidCellNew = cropImages( vidCell )
+%UNTITLED5 Summary of this function goes here
+%   Detailed explanation goes here
+
+    vidCellNew = vidCell;
+    [~,~,k] = size(vidCell);
+    
+    [image,rect] = imcrop(vidCell{1,1,1});
+    vidCellNew{1,1,1} = image;
+    
+    for count = 2:k
+        image = imcrop(vidCell{1,1,count},rect);
+        vidCellNew{1,1,count} = image;
+    end
+end
+
