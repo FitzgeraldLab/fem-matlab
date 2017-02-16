4db3ebc7696ba0e97dd166ddbad9026daae59049
Matthew <mmdstanley@gmail.com>
2017-02-16 15:14:56 -0800

    Finished editing CropVid.m.  Need to test

--- NcorrAnalysis/CropVid.m
+++ NcorrAnalysis/CropVid.m
@@ -1,17 +1,25 @@
-function vidNames = CropVid( vidName,div )
+function lastNum = CropVid( vidName,div )
 %CROPVID Summary of this function goes here
 %   Detailed explanation goes here
 
     video = vision.VideoFileReader(vidName);
+    frameRate = video.SampleRate;
+    
     i = 0;
-    n = 0;
+    lastNum = 1;
+    name = 'video' + num2str(lastNum) + '.mov';
+    videoWriter = vision.VideoFileWriter(name,'FrameRate',frameRate);
     while ~isDone(video)
+        if any(div(:) == i) %check if current frame is in division
+            release(videoWriter)
+            lastNum = lastNum + 1;
+            name = 'video' + num2str(lastNum) + '.mov';
+            videoWriter = vision.VideoFileWriter(name,'FrameRate',frameRate);
+        end
         i = i + 1;
         image = step(vid);
-        if any(div(:) == i) %check if current frame is in division
-            n = n + 1;
-            name = 'video' + num2str(n) + '.mov';
-        end
+        step(videoWriter,image);
     end
-    release(video)
+    release(video);
+    release(videoWriter);
 end
