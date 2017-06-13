f887c7c7a12faa270dbc6568e46a74cbf8a3e8d9
MattStan <mmdstanley@gmail.com>
2017-06-12 20:42:17 -0700

    Forgot to add script to the commit

---
+++ NcorrAnalysis/Version2/test.m
@@ -0,0 +1,5 @@
+addpath(pwd);
+cd C:\Users\Matthew\Documents\Interships-Jobs\Fitzgerald
+addpath(pwd);
+
+PreAnalyze('slomo_1485922878.mov',[1,559;590,736;758,1152;1187,1294;1317,1795;1831,2411;2451,2983;3015,3865;3897,5636;5660,10993],40);
