b8f7a5ae2f6025bfe1dcb39a7741b3ac67e0853f
34-Matt <mstanley3@zagmail.gonzaga.edu>
2017-06-16 22:57:42 -0700

    Moved to make changes on own computer

--- NcorrAnalysis/Version2/test.m
+++ NcorrAnalysis/Version2/test.m
@@ -1,5 +1,7 @@
 addpath(pwd);
-cd C:\Users\Matthew\Documents\Interships-Jobs\Fitzgerald
+cd /home/mstanley3/Desktop/Second Videos
 addpath(pwd);
 
 PreAnalyze('slomo_1485922878.mov',[1,559;590,736;758,1152;1187,1294;1317,1795;1831,2411;2451,2983;3015,3865;3897,5636;5660,10993],40);
+PreAnalyze('slomo_1485926725.mov',
+PreAnalyze('slomo_1485930001.mov',
