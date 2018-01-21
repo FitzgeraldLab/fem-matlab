d0d49fc80216823265f22ec67bc327515d56e549
Matthew <mmdstanley@gmail.com>
2018-01-20 18:53:57 -0800

    Created to get information from videos.  openNcorr not started

---
+++ ReadFile/openNcorr.m
@@ -0,0 +1,8 @@
+function [EXX,EYY,EXY,units] = openNcorr(x, y)
+%openTensileHueghs Takes text file from Hueghs and converts to readible file
+%   INPUT:
+%       name is the file name
+    %%
+    % Open Files
+    
+end
