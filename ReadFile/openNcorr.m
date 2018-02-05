b755c61dbdfeb52f55f3913dbd2d54aa4f4e34e9
Matthew <mmdstanley@gmail.com>
2018-02-05 12:42:58 -0800

    Made to receive multiple x and y values and can return cur and ref values

--- ReadFile/openNcorr.m
+++ ReadFile/openNcorr.m
@@ -1,8 +1,9 @@
-function [EXX,EYY,EXY,unit] = openNcorr(name, x, y)
+function [EXX,EYY,EXY,unit] = openNcorr(name, x, y, cur)
 %openTensileHueghs Takes text file from Hueghs and converts to readible file
 %   INPUT:
 %       name is the file name
 %       x, y interested x and y coordinate
+%       formatted boolean if should use cur (otherwise, use ref)
 %   OUTPUT:
 %       EXX, EYY, EXY the strains at x,y
 %       units the ncorr strain units
@@ -10,15 +11,38 @@ function [EXX,EYY,EXY,unit] = openNcorr(name, x, y)
     % Open Files
     load(name);
     %%
+    % Format inputs
+    if length(x) < length(y)
+        sizeValues = length(x);
+    else
+        sizeValues = length(y);
+    end
+    
+    if ~exist('cur','var')
+        cur = 0;
+    end
+    %%
     % Get strains
-    size = length(data_dic_save.strains);
-    EXX = zeros(1,size);
-    EYY = zeros(1,size);
-    EXY = zeros(1,size);
-    for i = 1:size
-        EXX(i) = data_dic_save.strains(i).plot_exx_ref_formatted(y,x);
-        EYY(i) = data_dic_save.strains(i).plot_eyy_ref_formatted(y,x);
-        EXY(i) = data_dic_save.strains(i).plot_exy_ref_formatted(y,x);
+    sizeData = length(data_dic_save.strains);
+    EXX = zeros(1,sizeData);
+    EYY = zeros(1,sizeData);
+    EXY = zeros(1,sizeData);
+    if cur
+        for i = 1:sizeData
+            for j = sizeValues
+                EXX(i) = EXX(i) + data_dic_save.strains(i).plot_exx_cur_formatted(y(j),x(j));
+                EYY(i) = EYY(i) + data_dic_save.strains(i).plot_eyy_cur_formatted(y(j),x(j));
+                EXY(i) = EXY(i) + data_dic_save.strains(i).plot_exy_cur_formatted(y(j),x(j));
+            end
+        end
+    else
+        for i = 1:sizeData
+            for j = sizeValues
+                EXX(i) = EXX(i) + data_dic_save.strains(i).plot_exx_ref_formatted(y(j),x(j));
+                EYY(i) = EYY(i) + data_dic_save.strains(i).plot_eyy_ref_formatted(y(j),x(j));
+                EXY(i) = EXY(i) + data_dic_save.strains(i).plot_exy_ref_formatted(y(j),x(j));
+            end
+        end
     end
     unit = data_dic_save.dispinfo.units;
     unit = strcat(unit,'/',unit);
