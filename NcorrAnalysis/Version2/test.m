1a44f5bdffb8067abdf09618b17d97f68f0a4d32
34-Matt <mstanley3@zagmail.gonzaga.edu>
2018-02-20 20:14:29 -0800

    updated test

--- NcorrAnalysis/Version2/test.m
+++ NcorrAnalysis/Version2/test.m
@@ -2,10 +2,8 @@
 % Add path to fem-matlab
 addpath('/home/mstanley3/Documents/MATLAB/ImageProcessing/fem-matlab/NcorrAnalysis/Version2');
 
-FRAMESPLIT = 60;
 %%
 % Add path of video file
-
 %videoPath1 = '/home/mstanley3/Desktop/Second Videos';
 videoPath2 = '/home/mstanley3/Desktop/Second Videos';
 cd(videoPath2)
@@ -14,4 +12,15 @@ cd(videoPath2)
 name = 'sample2.mov';
 %Last section for sample 3 instead of sample 2
 PreAnalyze(name,[755,1155;1314,1798;1825,2420; ...
-            2450,2986;3012,3868;3894,5633;5660,11179],'sample2','fraction',10);
+            2450,2986;3012,3868;3894,5633;5660,11179],'sample2','fraction',1);
+PreAnalyze(name,[14131,14943],'sample3','fraction',1);
+
+name = 'sample8.mov';
+%First two for sample 8, third for sample 9, all chordwise
+PreAnalyze(name,[1,1983;2001,2468],'sample8','fraction',1);
+PreAnalyze(name,[4480,5510],'sample9','fraction',1);
+
+name = 'sample6.mov';
+%All for sample 6
+PreAnalyze(name,[1,1880;1909,4082;4116,6794;6825,10169; ...
+            10212,10403;10808,13812],'sample6','fraction',1);
